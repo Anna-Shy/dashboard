3ccb4dc75ca636c84fbd54ca130f7d29ee5e9e39
Anna-Shy <shylkina.ao@gmail.com>
2023-11-12 19:41:17 +0200

    upd: logic to count  startWorkDate

--- src/components/userCard/UserCard.tsx
+++ src/components/userCard/UserCard.tsx
@@ -1,41 +1,83 @@
 import { useState, useEffect } from 'react';
+import moment from 'moment';
 
-import { userInfoData } from '../../source/data/UserInfoData';
+import './userCard.scss';
 
-import './userCard.scss'
+interface Employee {
+  id: number;
+  img: string;
+  username: string;
+  startWorkDate: string;
+  position: string;
+  timeOfTask: string;
+  workDuration?: string;
+}
 
 export const UserCard = () => {
-    const [days, setDays] = useState(0);
-    const [months, setMonths] = useState(0);
-    const [years, setYears] = useState(0);
+  const [employees, setEmployees] = useState<Employee[]>([]);
 
-    return (
-        <>
-            {userInfoData.map(user => (
-                <div className="box userInfo" key={user.id}>
-                    <div className="userInfo-title">
-                        <img className="icon" src={user.img} alt="icon" />
-                        <h2 className="name">{user.username}</h2>
-                    </div>
+  useEffect(() => {
+    fetch('src/source/data/UserInfoData.json')
+      .then(response => response.json())
+      .then(data => setEmployees(data))
+      .catch(error => console.error('Error loading data:', error));
+  }, []);
 
-                    {years < 0 ?
-                        <p className='userInfo-workData'> {months} m {days} d</p> :
-                        <p className='userInfo-workData'>  {years} years {months} mon {days} days</p>}
+  const calculateWorkDuration = (startWorkDate: string): string => {
+    const start = moment(startWorkDate);
+    const today = moment();
 
-                    <div className="box-row">
-                        <p className="userInfo-position">{user.position}</p>
+    const duration = moment.duration(today.diff(start));
 
-                        <div className="userInfo-timeOfTask">
-                            <span className="timeOfTask-color"
-                                style={{ color: Number(user.timeOfTask) < 20 ? 'tomato' : 'limegreen' }}>
-                                {user.timeOfTask}
-                            </span>
-                            <span className="timeOfTask-duration">this month</span>
-                        </div>
-                    </div>
-                </div>
-            ))
-            }
-        </>
-    )
+    const years = duration.years();
+    const months = duration.months();
+    const days = duration.days();
+
+    if (years === 0) {
+      return `${months} month ${days} days`;
+    } else {
+      return `${years} years ${months} month ${days} days`;
+    }
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setEmployees((employees: any) =>
+        employees.map((user: any) => ({
+          ...user,
+          workDuration: calculateWorkDuration(user.startWorkDate),
+        }))
+      );
+    }, 86400000); // 86400000 milisec in day
+
+    return () => clearInterval(intervalId);
+  }, [employees]);
+
+  return (
+    <>
+      {employees.map(user => (
+        <div className="box userInfo" key={user.id}>
+          <div className="userInfo-title">
+            <img className="icon" src={user.img} alt="icon" />
+            <h2 className="name">{user.username}</h2>
+          </div>
+
+          <p className='userInfo-workData'>{calculateWorkDuration(user.startWorkDate)}</p>
+
+          <div className="box-row">
+            <p className="userInfo-position">{user.position}</p>
+
+            <div className="userInfo-timeOfTask">
+              <span className="timeOfTask-color"
+                style={{ color: Number(user.timeOfTask) < 20 ? 'tomato' : 'limegreen' }}>
+                {user.timeOfTask}
+              </span>
+              <span className="timeOfTask-duration">this month</span>
+            </div>
+          </div>
+        </div>
+      ))
+      }
+    </>
+  )
 }
