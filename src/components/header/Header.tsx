a11af8c5c75330dba32caadc247431b984622c39
Anna-Shy <shylkina.ao@gmail.com>
2023-11-17 20:23:24 +0200

    upd: add menu btn component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import { Menu } from '../menu/Menu';
+import { Menu } from '../Menu/Menu';
 import logo from '../../source/svg/logo.svg';
 
 import './header.scss'
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,74 +1,12 @@
-import React, { useState } from 'react';
-import { Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@mui/material';
-
+import { MenuBtnAlert } from '../menuBtnAlert/MenuBtnAlert';
+import { MenuBtnDrawer } from '../menuBtnDrawer/MenuBtnDrawer';
 import './menu.scss';
 
 export const Menu = () => {
-    const [open, setOpen] = useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    const AlertInfo = () => {
-        return (
-            <React.Fragment>
-                <Dialog
-                    open={open}
-                    onClose={handleClose}
-                    aria-labelledby="alert-dialog-title"
-                    aria-describedby="alert-dialog-description"
-                >
-                    <DialogTitle id="alert-dialog-title">
-                        {"Attention"}
-                    </DialogTitle>
-                    <DialogContent>
-                        <DialogContentText id="alert-dialog-description">
-                            Been soon ...
-                            Just wait)) 
-                        </DialogContentText>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={handleClose} autoFocus>
-                            Close
-                        </Button>
-                    </DialogActions>
-                </Dialog>
-            </React.Fragment>
-        )
-    }
-
     return (
         <nav className="menu">
-            <React.Fragment>
-                <Button
-                    disabled={false}
-                    size="medium"
-                    variant="outlined"
-                    className='menu-btn'
-                    onClick={handleClickOpen}
-                >
-                    Info
-                </Button>
-                {AlertInfo()}
-            </React.Fragment>
-            <React.Fragment>
-                <Button
-                    disabled={false}
-                    size="medium"
-                    variant="outlined"
-                    className='menu-btn'
-                    onClick={handleClickOpen}
-                >
-                    Update data
-                </Button>
-                {AlertInfo()}
-            </React.Fragment>
+            <MenuBtnDrawer title={'Info'} />
+            <MenuBtnAlert title={'Update data'} />
         </nav>
     )
 }
-
